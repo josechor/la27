7239d36bce92d5766ae9129dbcefae9702413841
Rubén Blanco <blancolageruben@gmail.com>
2024-12-14 02:38:12 +0100

    docker compose bueno de verdad de la buena

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,9 @@
 import mysql from 'mysql2';
 
 export const pool = mysql.createPool({
-    host: 'database',
-    user: 'user',
-    password: 'userpassword',
-    database: 'mi_base_de_datos',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     port: 3306
 }).promise();
