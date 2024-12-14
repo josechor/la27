26ecc6dffdb81ac505729253d52bcef3df61bcbf
josechor <joserrapaz5@gmail.com>
2024-12-14 15:16:55 +0100

    save user password

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -20,11 +20,9 @@ const createUser = async (req, res) => {
   try {
     const { email, demon_name, user_name, password } = req.body;
     const hasedPassword = await hash(password, 10);
-
-    console.log(hasedPassword);
     const query =
-      "INSERT INTO demons (id, email, demon_name, user_name) VALUES (?, ?, ?, ?)";
-    await pool.query(query, [crypto.randomUUID(), email, demon_name, user_name]);
+      "INSERT INTO demons (id, email, demon_name, user_name, password) VALUES (?, ?, ?, ?, ?)";
+    await pool.query(query, [crypto.randomUUID(), email, demon_name, user_name, hasedPassword]);
     console.log("User created successfully");
 
     res.status(200).json({ message: "User created successfully" });
@@ -54,9 +52,7 @@ const authUser = async (req, res) => {
     if (!isPasswordMatch) {
       return res.status(401).json({ message: "Invalid credentials" });
     }
-    console.log(user)
-    delete user.password;
-    res.json(user);
+    res.json({data: user.id});
   } catch (error) {
     res.status(500).json({ message: "Internal server error: ", error });
   }
