9b0b7501b7d3361ff6ce9ee1094bdcc65cbe18dd
josechor <joserrapaz5@gmail.com>
2024-12-15 04:42:17 +0100

    check if user like tuip

--- src/controllers/tuipController.js
+++ src/controllers/tuipController.js
@@ -26,11 +26,47 @@ const getTuips = async (req, res) => {
       "SELECT tuips.id as tuipId, tuips.content as tuipContent, tuips.multimedia as tuipMultimedia, tuips.created_at as tuipCreatedAt, demons.id as demonId, demons.user_name as userName, demons.demon_name as demonName FROM tuips INNER JOIN demons ON tuips.demon_id = demons.id ORDER BY tuips.created_at DESC LIMIT ? OFFSET ?",
       [limit, (page - 1) * limit]
     );
-    for(const row of rows) {
-      const [total] = await pool.query("SELECT COUNT(*) as magrada FROM magrada where tuip_id = ?", [row.tuipId]);
+    for (const row of rows) {
+      const [total] = await pool.query(
+        "SELECT COUNT(*) as magrada FROM magrada where tuip_id = ?",
+        [row.tuipId]
+      );
+      const [didYouLiked] = await pool.query(
+        "SELECT * FROM magrada WHERE tuip_id = ? AND demon_id = ?",
+        [rows.tuipId, req.userData.id]
+      );
       row.magrada = total[0].magrada;
+      row.youLiked = didYouLiked.length > 0;
     }
     res.json(rows);
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error: ", error });
+  }
+};
+
+const setLike = async (req, res) => {
+  try {
+    const tuipId = req.params.id;
+    const demonId = req.userData.id;
+    await pool.query("INSERT INTO magrada (tuip_id, demon_id) VALUES (?, ?)", [
+      tuipId,
+      demonId,
+    ]);
+    res.status(201).json({ message: "Tuip liked successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error: ", error });
+  }
+};
+
+const removeLike = async (req, res) => {
+  try {
+    const tuipId = req.params.id;
+    const demonId = req.userData.id;
+    await pool.query("DELETE FROM magrada WHERE tuip_id = ? AND demon_id = ?", [
+      tuipId,
+      demonId,
+    ]);
+    res.status(200).json({ message: "Tuip unliked successfully" });
   } catch (error) {
     res.status(500).json({ message: "Internal server error: ", error });
   }
@@ -49,4 +85,4 @@ const createTuip = async (req, res) => {
   }
 };
 
-export { getTuip, createTuip, getTuips };
+export { getTuip, createTuip, getTuips, setLike, removeLike };
--- src/routes/tuipRoutes.js
+++ src/routes/tuipRoutes.js
@@ -3,13 +3,17 @@ import {
   getTuip,
   createTuip,
   getTuips,
+  setLike,
+  removeLike,
 } from "../controllers/tuipController.js";
 import authMiddleware from "../middlewares/authMiddleware.js";
 
 const tuipRoutes = Router();
 
-tuipRoutes.get("/", getTuips);
+tuipRoutes.get("/", authMiddleware, getTuips);
 tuipRoutes.get("/:id", getTuip);
 tuipRoutes.post("/", authMiddleware, createTuip);
+tuipRoutes.post("/like/:id", authMiddleware, setLike);
+tuipRoutes.delete("/like/:id", authMiddleware, removeLike);
 
 export { tuipRoutes };
