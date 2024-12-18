adb3f4ca2ddacd86a490e51c2550487b27ff403e
Jose Ramon <jrpaz@boardfy.com>
2024-12-18 12:29:50 +0100

    route /multimedia to get multimedia from server

--- src/app.js
+++ src/app.js
@@ -3,12 +3,18 @@ import { userRoutes } from "./routes/userRoutes.js";
 import { tuipRoutes } from "./routes/tuipRoutes.js";
 import { groupRoutes } from "./routes/groupRoutes.js";
 import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(json());
 app.use(cors({ origin: "*" }));
 
+app.use('/multimedia', express.static(path.join(__dirname, '../uploads')));
 app.use("/api/users", userRoutes);
 app.use("/api/tuips", tuipRoutes);
 app.use("/api/groups", groupRoutes);
